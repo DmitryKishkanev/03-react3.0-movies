7264a7c723daa994b3504733c9dba00cbc92013b
DmitryKishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2026-07-02 19:29:33 +0300

    добавил компонент MovieItem

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,76 @@
+import { useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
+import type { Movie } from "@/types/movie";
+import SearchBar from "@/components/SearchBar";
+import MovieGrid from "@/components/MovieGrid";
+import Loader from "@/components/Loader";
+import ErrorMessage from "@/components/ErrorMessage";
+import MovieModal from "@/components/MovieModal";
+import MovieItem from "@/components/MovieItem";
+import fetchMovies from "@/services";
 import css from "./App.module.css";
 
 function App() {
-  return <div className={css.container}></div>;
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSearch = async (query: string) => {
+    try {
+      setIsLoading(true);
+      setIsError(false);
+      setMovies([]);
+      const data = await fetchMovies({ query: query });
+      if (data.movies.length === 0) {
+        toast("No movies found for your request", {
+          style: {
+            background: "#a20e0e",
+            color: "#fff",
+          },
+        });
+      }
+      setMovies(data.movies);
+    } catch (error) {
+      console.error("Ошибка при загрузке:", error);
+      setIsError(true);
+      setMovies([]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSelect = (movie: Movie) => {
+    setSelectedMovie(movie);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedMovie(null);
+  };
+
+  return (
+    <div className={css.container}>
+      <SearchBar onSubmit={handleSearch} />
+      {isLoading && <Loader message="Loading movies, please wait..." />}
+
+      {isError && (
+        <ErrorMessage message="There was an error, please try again..." />
+      )}
+
+      {movies && <MovieGrid movies={movies} onSelect={handleSelect} />}
+
+      {isModalOpen && selectedMovie && (
+        <MovieModal onClose={closeModal}>
+          <MovieItem movie={selectedMovie} />
+        </MovieModal>
+      )}
+
+      <Toaster position="top-center" reverseOrder={false} />
+    </div>
+  );
 }
 
 export default App;
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import type { Movie } from "@/types/movie";
 
 const BASE_URL = "https://api.themoviedb.org/3/search/movie";
-const myKey = import.meta.env.VITE_API_KEY;
+const myKey = import.meta.env.VITE_TMDB_TOKEN;
 
 interface FatchMoviesParams {
   query: string;
   page?: number;
-  include_adult: boolean;
+  include_adult?: boolean;
   language?: string;
 }
 
